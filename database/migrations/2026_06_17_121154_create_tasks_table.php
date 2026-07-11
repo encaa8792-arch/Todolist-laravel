37e1b3daa3587adc65b3d199c04a2272dcba8df2
encaa8792-arch <encaa8792@gmail.com>
2026-07-11 11:06:19 +0700

    fix: hapus kolom is_done dari task

--- database/migrations/2026_06_17_121154_create_tasks_table.php
+++ database/migrations/2026_06_17_121154_create_tasks_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('task');
-            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
